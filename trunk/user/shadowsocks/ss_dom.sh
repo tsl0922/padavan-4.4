898fd62ae73cc63ccfd130e6e5b8041c623b5a1a
achangqing <95298467+achangqing@users.noreply.github.com>
2024-11-09 21:52:28 +0800

    强制走SS的域名

--- trunk/user/shadowsocks/ss_dom.sh
+++ trunk/user/shadowsocks/ss_dom.sh
@@ -1,9 +1,1 @@
-https://raw.githubusercontent.com/v2fly/domain-list-community/release/gfwlist.txt
-https://cdn.jsdelivr.net/gh/v2fly/domain-list-community@release/gfwlist.txt
 
-https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
-https://pagure.io/gfwlist/raw/master/f/gfwlist.txt
-http://repo.or.cz/gfwlist.git/blob_plain/HEAD:/gfwlist.txt
-https://bitbucket.org/gfwlist/gfwlist/raw/HEAD/gfwlist.txt
-https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt
-https://git.tuxfamily.org/gfwlist/gfwlist.git/plain/gfwlist.txt
